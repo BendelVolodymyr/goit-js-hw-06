eef388ca6cf36207d062043989bd1f7ebed44f7f
BendelVolodymyr <bendel.vova@gmail.com>
2023-09-12 21:54:16 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,8 +1,18 @@
+const bodyStyle = document.querySelector('body');
+const button = document.querySelector('.change-color');
+const nameColor = document.querySelector('span.color');
 
-
+const random = () => {
+  bodyStyle.style.cssText = ` background-color: ${getRandomHexColor()}`;
+  nameColor.innerHTML = getRandomHexColor();
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+button.addEventListener('click', random);
+
+console.log(getRandomHexColor())
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,14 @@
+const inputNumber = document.querySelector('input');
+const buttonDestroy = document.querySelector('button[data-destroy]');
+const buttonCreate = document.querySelector('button[data-create]');
+
+function createBoxes(amount) {
+
+};
+const inputResult = (event) => {
+
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
