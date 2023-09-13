86a4daf30632db794a68b14c3591bb71d9ec9a2c
BendelVolodymyr <bendel.vova@gmail.com>
2023-09-13 22:29:21 +0200

    push task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,13 +1,43 @@
 const inputNumber = document.querySelector('input');
+const div = document.querySelector('div#boxes');
 const buttonDestroy = document.querySelector('button[data-destroy]');
 const buttonCreate = document.querySelector('button[data-create]');
+div.style.cssText = `display: flex;
+flex-wrap: wrap;
+row-gap: 20px;
+gap: 20px;`
+
+
+function destroyBoxes() {
+  inputNumber.value = '';
+ div.innerHTML = '';
+}
 
 function createBoxes(amount) {
-
+  let total = 30;
+  const result = [];
+  for (let i = 0; i < amount; i += 1) {
+    const newDiv = document.createElement('div');
+    total += 10;
+      div.append(newDiv);
+      newDiv.style.cssText = `background-color: ${getRandomHexColor()};
+      width: ${total}px;
+      height: ${total}px;
+      `;
 };
-const inputResult = (event) => {
-
 };
+const inputResult = () => {
+  if ( Number(inputNumber.value) < Number(inputNumber.min) || Number(inputNumber.value) > Number(inputNumber.max)) {
+    alert(`    Min numbers: ${inputNumber.min} 
+    Max numbers: ${inputNumber.max}`);
+  } else {
+    createBoxes(inputNumber.value.trim());
+    }
+  }
+
+buttonCreate.addEventListener('click', inputResult);
+
+buttonDestroy.addEventListener('click', destroyBoxes);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
